f7b7860c7a1668021ce7171624160d8b509df0a8
daback <daback93@gmail.com>
2024-02-05 14:22:32 +0900

    daback 0205 수업내용 반영 commit

--- src/main/java/kr/withbooks/web/context/MybatisContext.java
+++ src/main/java/kr/withbooks/web/context/MybatisContext.java
@@ -1,6 +1,11 @@
 package kr.withbooks.web.context;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import jakarta.servlet.http.HttpServlet;
 
@@ -10,6 +15,20 @@ public class MybatisContext{
 	
 
 	private static SqlSessionFactory sqlSessionFactory;
+	
+	static {
+		System.out.println("스태틱 실행");
+		try {
+			String resource= "mybatis-config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			MybatisContext.setSqlSessionFactory(sqlSessionFactory);
+		} catch (IOException e) {
+			System.out.print("예외발생");
+			e.printStackTrace();
+		}
+		
+	}
 
 	public static SqlSessionFactory getSqlSessionFactory() {
 		return sqlSessionFactory;
---
+++ src/main/java/kr/withbooks/web/listener/MybatisContextListener.java
@@ -0,0 +1,27 @@
+package kr.withbooks.web.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class MybatisContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("톰캣이 시작되는겨 ???");
+		
+		try {
+			Class.forName("kr.withbooks.web.context.MybatisContext");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("톰캣이 꺼지는겨 ???");
+	}
+	
+}
